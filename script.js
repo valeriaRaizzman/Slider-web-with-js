70d95dda277d095341dd506f957daffc18aa6ee2
Valeria Raizman <132442225+valeriaRaizzman@users.noreply.github.com>
2023-06-21 14:52:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+// script.js
+
+// Selecting elements
 const left = document.querySelector('.left')
 const right = document.querySelector('.right')
 const cont = document.querySelector('.container')
 
+// Adding event listeners for mouseenter and mouseleave
 left.addEventListener('mouseenter', () => { cont.classList.add('hover-left') })
 left.addEventListener('mouseleave', () => { cont.classList.remove('hover-left') })
 
